b58973893c40f4a58b8a3a0eff50572db2034d43
anhnn <anhnhatnguyen2103@gmail.com>
2022-12-26 11:18:48 +0700

    header PC

--- LaTha/src/app/Components/shared/header/header.component.ts
+++ LaTha/src/app/Components/shared/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  user=[
+    {
+      pic:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png",
+      name:"Chu Hoàng Minh",
+    },
+  ]
 }
